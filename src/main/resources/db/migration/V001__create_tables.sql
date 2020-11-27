0fe59b8076578173dc9e70944cd5c9016961c2b7
Heitor Elias Teixeira <heitor.teixeira@hotmail.com>
2020-11-26 21:42:59 -0300

    Add database to the project

---
+++ src/main/resources/db/migration/V001__create_tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE city (
+	id INT auto_increment NOT NULL,
+	name varchar(100) NOT NULL,
+	state char(2) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE client (
+	id INT auto_increment NOT NULL,
+	name varchar(100) NOT NULL,
+	gender char(1) NULL,
+	birth DATE NULL,
+	age varchar(100) NULL,
+	city_id INT NULL,
+	CONSTRAINT FK_client_city FOREIGN KEY (city_id) REFERENCES city (id),
+	PRIMARY KEY (id)
+);
